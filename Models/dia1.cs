45c12c3626391c9502cceb5a68bff8ec6bd68604
LucaaassDev <lucasgonzagafranco19@gmail.com>
2025-06-02 21:46:26 -0300

    Primeiro commit

---
+++ Models/dia1.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Exercicios
+{
+    public class Dia1
+{
+    public void ExibirMensagem()
+    {
+        string nome = "Lucas";
+
+        Console.WriteLine($"Seja Bem-vindo ao C#, {nome} ");
+    }
+}
+
+public class Dia2
+{
+    public void ExibirInfo()
+    {
+        string nome = "Lucas";
+        char letra = 'A';
+        bool verdadeiro = true;
+        double peso = 56.5;
+        float altura = 1.70f;
+        int idade = 21;
+
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Inicial: {letra}");
+        Console.WriteLine($"Voce é Desenvolvedor? {verdadeiro}");
+        Console.WriteLine($"Peso: {peso}");
+        Console.WriteLine($"Altura: {altura}");
+        Console.WriteLine($"Idade: {idade}");
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Exercicios;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dia1 dia1 = new Dia1();
+            dia1.ExibirMensagem();
+
+            Console.WriteLine();
+
+            Dia2 dia2 = new Dia2();
+            dia2.ExibirInfo();
+        }
+    }
+}
+
